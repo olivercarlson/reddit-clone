e9aeece722b560c79cbe23d645d794f5880d8dee
Oliver Carlson <oswain@gmail.com>
2020-10-14 15:25:33 -0700

    get posts and get postById GQL added

---
+++ src/entities/Post.ts
@@ -0,0 +1,22 @@
+import { Property, Entity, PrimaryKey } from '@mikro-orm/core';
+import { Field, ObjectType, Int } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class Post {
+	@Field(() => Int)
+	@PrimaryKey()
+	id!: number;
+
+	@Field(() => String)
+	@Property({ type: 'date' })
+	createdAt = new Date();
+
+	@Field(() => String)
+	@Property({ type: 'date', onUpdate: () => new Date() })
+	updatedAt = new Date();
+
+	@Field()
+	@Property({ type: 'text' })
+	title!: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
+import microConfig from './mikro-orm.config';
+// import { Post } from './entities/Post';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
-	const orm = await MikroORM.init({ dbName: 'lireddit', type: 'postgresql', debug: process.env.NODE_ENV !== 'production' });
+	const orm = await MikroORM.init(microConfig);
+	orm.getMigrator().up();
+	const app = express();
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({ resolvers: [HelloResolver, PostResolver], validate: false }),
+		context: () => ({ em: orm.em }),
+	});
+
+	apolloServer.applyMiddleware({ app });
+
+	app.listen(4000, () => {
+		console.log('server started on localhost:4000');
+	});
 };
 
-main();
+main().catch((err) => console.log(err));
